e9b76d9ab03080e358942b84159c8c96c2ad4da6
Szymon J <szymon.j@hotmail.com>
2018-10-18 17:31:02 +0100

    Updated code

--- messingWithJSON.py
+++ messingWithJSON.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3 //Runs on Python 3
-makeTextFile = open("testFile.txt","w+") #Write permission, creating a file if it doesn't exist
+def makeTextFile(): #Define function
+    open("testFile.txt","w+") #Write permission, creating a file if it doesn't exist
+
+makeTextFile()
