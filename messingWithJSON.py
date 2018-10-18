a03504e7c93500bd07d9985ad975bf7a18a18c37
Szymon J <szymon.j@hotmail.com>
2018-10-18 20:05:33 +0100

    Started to implement save limiting

--- messingWithJSON.py
+++ messingWithJSON.py
@@ -6,6 +6,7 @@
 #Store this data in the JSON database
 #Possible data redundancy check before the new input data is stored
 #Need to check amount of lines present in text file, and create a new entry on a new line.
+#only allow current session/same data to be saved once
 
 import os
 import webbrowser
@@ -13,20 +14,19 @@ import json
 import time
 from colour import Color
 
-
 fileName = "testJSONFile.json" #Used for testing, referenced later in code
 errorMessage = "Cancelling operation." #Convenience to change error code. Used when no correct phrase is entered.
+timesFileSaved = 0 #Will be used to only allow user to save once per session.
 
 def makeTextFile(): #Define function
     open(fileName,"w+") #Write permission, creating a file if it doesn't exist
-    open(fileName,"w+").close() #closes the file
+    open(fileName,"w+").close() #closes the file. Possibly makes previous line redundant
 
 if os.path.exists(fileName): #Checks if JSON file exists
     print(fileName,"already exists.")
 else:
+    print(fileName,"does not exist, but has now been created.")
     makeTextFile() #only create a new file if no file exists.
-
-#makeTextFile() #Have to change this to checking if file ezists first, or else it overwrites information
 
 userName = input("What's your name?")
 userSurname = input("What's your surname?")
@@ -41,6 +41,7 @@ def mainMenuFunc(): #Function used for the main menu.
 #            open(fileName,"w").write(userName) #"w" replaces the whole file. Must change.
             open(fileName,"a").write(userName) #Appends the file instead.
             print(fileName,"was saved.")
+#            timesFileSaved += 1 #Incrementally add 1 to prevent saving more than once
             mainMenuFunc() #Referenced to repeat the code
         else:
             print(errorMessage)
