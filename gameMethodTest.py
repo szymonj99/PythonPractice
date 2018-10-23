ee0af502d990a48faf6f31e50902fd3627296d18
Szymon J <32754152+szymonj99@users.noreply.github.com>
2018-10-23 03:50:38 +0100

    added two methods

---
+++ gameMethodTest.py
@@ -0,0 +1,10 @@
+def methodOne():
+    print("This is method one.")
+    methodTwo()
+
+def methodTwo():
+    print("This is method two.")
+
+methodOne()
+
+#Quick test. Prints out two sentences as it should
