fdc6b91ed0b5121916f3cc3a23621c9aa1b7edc2
Szymon J <32754152+szymonj99@users.noreply.github.com>
2018-10-23 04:26:44 +0100

    split character class selection into multiple methods

--- simpleGameTest.py
+++ simpleGameTest.py
@@ -10,7 +10,11 @@ fileName = "saveFile.json"
 errorMessage = "Error. Unknown or unexpected input."
 characterName = "startOfGamePlaceholderName" #global variable, not used correctly. Later function only changes value for local variable in function.
 characterClass = "startOfGamePlaceholderClass"
-
+confirmationYesInput = ("Yes", "yes", "Y", "y")
+confirmationNoInput = ("No", "no", "N", "n")
+confirmationWarriorInput = ("Warrior", "warrior")
+confirmationArcherInput = ("Archer", "archer")
+confirmationMageInput = ("Mage", "mage")
 
 def makeSaveFile(): #Define function
     open(fileName,"w+") #Write permission, creating a file if it doesn't exist
@@ -21,30 +25,43 @@ def makeCharacterMenu():
     global characterClass
     characterName = input("What would you like to be called?\n")
     characterClass = input("What class do you want to be? [Warrior/Archer/Mage]\n")
-    
-    if characterClass == "Warrior":
-        characterClassConfirmation = input("You have chosen Warrior. Are you sure? [Yes/Y/No]\n")
-        if characterClassConfirmation == "Yes" or characterClassConfirmation == "y":
-            print("Class confirmed.")
-            gameInterface()
-        else:
-            makeCharacterMenu()
-    
-    if characterClass == "Archer":
-        characterClassConfirmation = input("You have chosen Archer. Are you sure?\n")
-        if characterClassConfirmation == "Yes" or characterClassConfirmation == "y":
-            print("Class confirmed.")
-            gameInterface()
-        else:
-            makeCharacterMenu()
+    pickCharacterClass()
 
-    if characterClass == "Mage":
-        characterClassConfirmation = input("You have chosen Mage. Are you sure?\n")
-        if characterClassConfirmation == "Yes" or characterClassConfirmation == "y":
-            print("Class confirmed.")
-            gameInterface()
-        else:
-            makeCharacterMenu()
+def playerPickedWarrior():
+    characterClassConfirmation = input("You have chosen Warrior. Are you sure? [Yes/Y/No/N]\n")
+    if characterClassConfirmation in confirmationYesInput:
+        print("Class confirmed.")
+        gameInterface()
+    else:
+        makeCharacterMenu()
+
+def playerPickedArcher():
+    characterClassConfirmation = input("You have chosen Archer. Are you sure?\n")
+    if characterClassConfirmation in confirmationYesInput:
+        print("Class confirmed.")
+        gameInterface()
+    else:
+        makeCharacterMenu()
+
+def playerPickedMage():
+    characterClassConfirmation = input("You have chosen Mage. Are you sure?\n")
+    if characterClassConfirmation in confirmationYesInput:
+        print("Class confirmed.")
+        gameInterface()
+    else:
+        makeCharacterMenu()
+
+def pickCharacterClass():
+    global characterClass
+    if characterClass in confirmationWarriorInput:
+        playerPickedWarrior()        
+    
+    elif characterClass in confirmationArcherInput:
+        playerPickedArcher()        
+
+    elif characterClass in confirmationMageInput:
+        playerPickedMage()
+        
 
 def clearTerminal(): 
     if os.name == "nt": #Used if system is windows
@@ -71,9 +88,9 @@ def gameInterface():
 if os.path.exists(fileName): #Checks if JSON file exists
     print("Your save file,",fileName,"already exists.\nYour game will begin in 2 seconds.")
     time.sleep(2)
-    with open(fileName) as saveFile:    
-        saveData = json.load(saveFile)
-        pprint.pprint(saveData)
+#    with open(fileName) as saveFile:     #Prints save data for testing.
+#        saveData = json.load(saveFile)
+#        pprint.pprint(saveData)
 
 else:
     print("A save file",fileName,"does not exist, but has now been created.\n")
