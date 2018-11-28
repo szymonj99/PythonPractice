c5378e650c9fdf2433536dfcb01b94d87f4e291e
szymonj99 <szymon.j@hotmail.com>
2018-11-28 19:40:33 +0000

    Program is fully functional.

--- mathsAssignmentDiceRolls.py
+++ mathsAssignmentDiceRolls.py
@@ -3,6 +3,7 @@
 import random
 import time
 import math
+import sys
 
 #Input Granny numbers.
 #Input Your numbers
@@ -11,6 +12,10 @@ import math
 
 diceLowerLimit = 1
 diceHigherLimit = 6
+currentDiceRoll = 2
+
+yes = ("Y", "y", "yes", "Yes")
+no = ("N", "n", "no", "No")
 
 #Granny will have two dice, ONLY TWO numbers on each.
 #You will also have two dice, ONLY TWO numbers on each.
@@ -105,6 +110,7 @@ def youDiceTwoNumberTwoChoice():
         youDiceTwoNumberTwoChoice()
     elif (youDiceTwoNumberTwo > 0) and (youDiceTwoNumberTwo < 7):
         printBothDice() #Code should be changed to let the computer randomise the dice rolls.
+        diceRollRandomisationDecision() #Executes method defined later.
 
 def printYourDice():
     print("Your Dice 1: " + str(youDiceOneNumberOne) + "," + str(youDiceOneNumberTwo))
@@ -117,6 +123,65 @@ def printBothDice():
     printGrannyDice()
     print() #Leaves a space before dice output. Looks cleaner in the terminal.
     printYourDice()
+
+###DICE ROLLS RANDOMISATION###
+#Use modulus of 2 to see if number is odd or even.
+#N = Roll number. Will start at 2, or any even number. Can't start at 2, 0%2. 
+#If the modulus 2 of N = 0, Granny rolls randomly, one number from both dice. Add 1 to N.
+#If the modulus 2 of N = 1, You roll randomly, one number from both dice. Add 1 to N.
+
+def diceRollRandomisationDecision():
+    global currentDiceRoll
+    print("Roll again? [Yes/No]")
+    userWantsNextRoll = input()
+    if userWantsNextRoll in yes:
+        if (currentDiceRoll % 2) == 0:
+            grannyRandomRoll()
+        elif (currentDiceRoll % 2) == 1:
+            youRandomRoll()
+    elif userWantsNextRoll in no:
+        sys.exit(0)
+    else:
+        print("Command not recognized.")
+        diceRollRandomisationDecision()
+
+def grannyRandomRoll():
+    global currentDiceRoll, grannyDiceOneNumberOne, grannyDiceOneNumberTwo, grannyDiceTwoNumberOne, grannyDiceTwoNumberTwo
+    diceOneRandomSelection = random.randint(1,2)
+    diceTwoRandomSelection = random.randint(1,2)
+    if (diceOneRandomSelection == 1 and diceTwoRandomSelection == 1):
+        grannyTotal = grannyDiceOneNumberOne + grannyDiceTwoNumberOne
+        print("Granny rolled:" , grannyTotal)
+    if (diceOneRandomSelection == 1 and diceTwoRandomSelection == 2):
+        grannyTotal = grannyDiceOneNumberOne + grannyDiceTwoNumberTwo
+        print("Granny rolled:" , grannyTotal)
+    if (diceOneRandomSelection == 2 and diceTwoRandomSelection == 1):
+        grannyTotal = grannyDiceOneNumberTwo + grannyDiceTwoNumberOne
+        print("Granny rolled:" , grannyTotal)
+    if (diceOneRandomSelection == 2 and diceTwoRandomSelection == 2):
+        grannyTotal = grannyDiceOneNumberTwo + grannyDiceTwoNumberTwo
+        print("Granny rolled:" , grannyTotal)
+    currentDiceRoll += 1
+    diceRollRandomisationDecision()
+
+def youRandomRoll():
+    global currentDiceRoll, youDiceOneNumberOne, youDiceOneNumberTwo, youDiceTwoNumberOne, youDiceTwoNumberTwo
+    diceOneRandomSelection = random.randint(1,2)
+    diceTwoRandomSelection = random.randint(1,2)
+    if (diceOneRandomSelection == 1 and diceTwoRandomSelection == 1):
+        yourTotal = youDiceOneNumberOne + youDiceTwoNumberOne
+        print("You rolled:" , yourTotal)
+    if (diceOneRandomSelection == 1 and diceTwoRandomSelection == 2):
+        yourTotal = youDiceOneNumberOne + youDiceTwoNumberTwo
+        print("You rolled:" , yourTotal)
+    if (diceOneRandomSelection == 2 and diceTwoRandomSelection == 1):
+        yourTotal = youDiceOneNumberTwo + youDiceTwoNumberOne
+        print("You rolled:" , yourTotal)
+    if (diceOneRandomSelection == 2 and diceTwoRandomSelection == 2):
+        yourTotal = youDiceOneNumberTwo + youDiceTwoNumberTwo
+        print("You rolled:" , yourTotal)
+    currentDiceRoll += 1
+    diceRollRandomisationDecision()
 
 ###PROGRAM EXECUTION###
 
